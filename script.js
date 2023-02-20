9a17a1f8dc6d0052b36d31ff9c11517e0518b82b
Elena Tykhonova <sivak_elena@ukr.net>
2023-02-21 00:50:12 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+let num1 = prompt('Введіть перше число:', '0');
+let num2 = prompt('Введіть друге число:', '0');
+alert(`Користувач ввів ${num1} і ${num2}:
+        \u2022  ${num1}+${num2}=${Number(num1)+Number(num2)}
+        \u2022  ${num1}-${num2}=${Number(num1)-Number(num2)}
+        \u2022  ${num1}*${num2}=${Number(num1)*Number(num2)}
+        \u2022  ${num1}/${num2}=${Number(num1)/Number(num2)}`);
+
